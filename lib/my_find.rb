49eb8825e054dc0edc6d33b01328a3cfddb3db21
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-19 22:20:30 +0200

    Use while.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,9 @@
 require 'pry'
 
-def my_find(collection)
-  for item in collection
-    if yield(item)
-      return item
-    end
+def my_find(array)
+  i = 0
+  while i < array.size
+    return array[i] if yield(array[i])
+    i += 1
   end
-  return nil
 end
